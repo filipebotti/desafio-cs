d5d0dd723352a9c6e84fa649a7ae7ad59791d2ee
Filipe Botti <filipebotti@hotmail.com>
2017-01-14 18:33:29 -0300

    adjust responseHelper to the new form

--- app/shared/responseHelper.js
+++ app/shared/responseHelper.js
@@ -1,19 +1,19 @@
-const Q             = require('q')
-const HttpStatus    = require('http-status')
-const Strings       = require('./strings')
-const debug         = require('./debugger')('shared:responseHelper')
+const Q             = require('q');
+const HttpStatus    = require('http-status');
+const Strings       = require('./strings');
+const debug         = require('./debugger')('shared:responseHelper');
 
 function defaultResponse(data, statusCode = HttpStatus.OK) {
 
-    debug('status: %j - data: %j', statusCode, data)    
-    return Q({ data, statusCode })
+    debug('status: %j - data: %j', statusCode, data); 
+    return Q({ data, statusCode });
 }
 
 function errorResponse(mensagem, statusCode = HttpStatus.INTERNAL_SERVER_ERROR) {
 
     if(statusCode == HttpStatus.INTERNAL_SERVER_ERROR) {   
         
-        mensagem = Strings.get('internal-server-error')
+        mensagem = Strings.INTERNAL_SERVER_ERROR;
     }
 
     return defaultResponse({ mensagem }, statusCode);
