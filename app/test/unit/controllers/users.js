3def764a116934767a9a6ad7195cddd57b893eed
Filipe Botti <filipebotti@hotmail.com>
2017-01-14 17:45:37 -0300

    change error property to mensagem property

--- app/test/unit/controllers/users.js
+++ app/test/unit/controllers/users.js
@@ -4,6 +4,7 @@ const Users = require('../../../models/users');
 const UsersController = require('../../../controllers/users');
 const expect = require('chai').expect;
 const Strings = require('../../../shared/strings');
+const debug = require('../../../shared/debugger')('unitTest-controllers:users');
 
 describe('Controllers: Users', () => {
     
@@ -25,12 +26,13 @@ describe('Controllers: Users', () => {
             return _usersController
                         .create(data)
                         .then(result => {
+                            debug(result);
                             expect(result.statusCode).to.exist;
                             expect(result.statusCode).to.be.eql(HttpStatus.BAD_REQUEST);
                             expect(result.data).to.be.an('object');
-                            expect(result.data.error).to.be.a('string');
-                            expect(result.data.error).not.be.empty;
-                            expect(result.data.error).to.be.eql(Strings.NOME_FIELD_NOT_FOUND)
+                            expect(result.data.mensagem).to.be.a('string');
+                            expect(result.data.mensagem).not.be.empty;
+                            expect(result.data.mensagem).to.be.eql(Strings.NOME_FIELD_NOT_FOUND)
 
                         });
         });
