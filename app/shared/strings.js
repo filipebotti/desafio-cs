20ab8a2cad28fdef834c90db70b4221ac538d036
Filipe Botti <filipebotti@hotmail.com>
2017-01-14 18:33:58 -0300

    new error messages added

--- app/shared/strings.js
+++ app/shared/strings.js
@@ -3,6 +3,8 @@ const content = {
     EMAIL_FIELD_NOT_FOUND       : "O email não foi informado",
     SENHA_FIELD_NOT_FOUND       : "A senha não foi informada",
     TELEFONE_FIELD_NOT_FOUND    : "Nenhum telefone foi informado",
+    TELEFONE_FIELD_AT_LEAST_ONE : "Deve ser informado pelo menos um telefone",
+    INTERNAL_SERVER_ERROR       : "Ops! Algo não funcionou como esperado."
 };
 
 module.exports = content;
