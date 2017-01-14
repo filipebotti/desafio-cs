7feebc458fddf08046debb74464d03ef3c60c6d8
Filipe Botti <filipebotti@hotmail.com>
2017-01-14 18:19:00 -0300

    unit test added if telefones not passed

--- app/shared/strings.js
+++ app/shared/strings.js
@@ -1,7 +1,8 @@
 const content = {
-    NOME_FIELD_NOT_FOUND  : "O nome não foi informado",
-    EMAIL_FIELD_NOT_FOUND : "O email não foi informado",
-    SENHA_FIELD_NOT_FOUND : "A senha não foi informada"
+    NOME_FIELD_NOT_FOUND        : "O nome não foi informado",
+    EMAIL_FIELD_NOT_FOUND       : "O email não foi informado",
+    SENHA_FIELD_NOT_FOUND       : "A senha não foi informada",
+    TELEFONE_FIELD_NOT_FOUND    : "Nenhum telefone foi informado",
 };
 
 module.exports = content;
--- app/test/unit/controllers/users.js
+++ app/test/unit/controllers/users.js
@@ -79,6 +79,27 @@ describe('Controllers: Users', () => {
                             expect(result.data.mensagem).to.be.eql(Strings.SENHA_FIELD_NOT_FOUND)
                         });
         });
+
+        it('should return an error message if telefones is not passed', () => {
+            
+            const data = {
+                nome : "nome",
+                email: "email",
+                senha: "senha",
+            };
+
+            return _usersController
+                        .create(data)
+                        .then(result => {
+                            debug(result);
+                            expect(result.statusCode).to.exist;
+                            expect(result.statusCode).to.be.eql(HttpStatus.BAD_REQUEST);
+                            expect(result.data).to.be.an('object');
+                            expect(result.data.mensagem).to.be.a('string');
+                            expect(result.data.mensagem).not.be.empty;
+                            expect(result.data.mensagem).to.be.eql(Strings.TELEFONE_FIELD_NOT_FOUND)
+                        });
+        });
     });
 
 })
