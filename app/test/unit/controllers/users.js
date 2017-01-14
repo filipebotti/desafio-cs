625a7b5bb7c6cc8b72a5c3bc43a537208d48f7d5
Filipe Botti <filipebotti@hotmail.com>
2017-01-14 18:33:47 -0300

    unit test to telefones have at least one

--- app/test/unit/controllers/users.js
+++ app/test/unit/controllers/users.js
@@ -1,10 +1,10 @@
-const HttpStatus = require('http-status');
-const Q = require('q');
-const Users = require('../../../models/users');
-const UsersController = require('../../../controllers/users');
-const expect = require('chai').expect;
-const Strings = require('../../../shared/strings');
-const debug = require('../../../shared/debugger')('unitTest-controllers:users');
+const HttpStatus        = require('http-status');
+const Q                 = require('q');
+const Users             = require('../../../models/users');
+const UsersController   = require('../../../controllers/users');
+const { expect }        = require('chai');
+const Strings           = require('../../../shared/strings');
+const debug             = require('../../../shared/debugger')('unitTest-controllers:users');
 
 describe('Controllers: Users', () => {
     
@@ -98,6 +98,28 @@ describe('Controllers: Users', () => {
                             expect(result.data.mensagem).to.be.a('string');
                             expect(result.data.mensagem).not.be.empty;
                             expect(result.data.mensagem).to.be.eql(Strings.TELEFONE_FIELD_NOT_FOUND)
+                        });
+        });
+
+        it('should return an error message if telefones dont have at least one', () => {
+            
+            const data = {
+                nome : "nome",
+                email: "email",
+                senha: "senha",
+                telefones: []
+            };
+
+            return _usersController
+                        .create(data)
+                        .then(result => {
+                            debug(result);
+                            expect(result.statusCode).to.exist;
+                            expect(result.statusCode).to.be.eql(HttpStatus.BAD_REQUEST);
+                            expect(result.data).to.be.an('object');
+                            expect(result.data.mensagem).to.be.a('string');
+                            expect(result.data.mensagem).not.be.empty;
+                            expect(result.data.mensagem).to.be.eql(Strings.TELEFONE_FIELD_AT_LEAST_ONE)
                         });
         });
     });
