005515c1ee9148dd68a8404a3f8b913fa178bc3b
Filipe Botti <filipebotti@hotmail.com>
2017-01-14 15:16:47 -0300

    added first logic to users post route

---
+++ app/routes/users.js
@@ -0,0 +1,15 @@
+const UsersController = require('../controllers/users');
+const Users = require('../models/users');
+
+module.exports = (app) => {
+    
+    const usersController = UsersController(Users);
+
+    app
+        .route('/users')
+        .post((request, response) => {
+            usersController
+                .create(request.body)
+                .then(res => response.status(res.statusCode).send(res.data));
+        })
+};
