31c12c815a880ba06c77e1fd63ed9eb2ba30eaf4
Filipe Botti <filipebotti@hotmail.com>
2017-01-15 14:01:37 -0300

    added tes to users model for isPasswordEqual()

--- app/test/unit/models/users.js
+++ app/test/unit/models/users.js
@@ -18,6 +18,26 @@ describe('Models: Users', () => {
             expect(password).to.exist;
             expect(password).to.be.a('string');
             expect(password).to.be.eql('D3BCEC7CE184C002B1CE7805CF36F333'); //md5 for 123desafio
+        });        
+    });
+
+    describe('isPasswordEqual()', () => {
+
+        it('should exist and be a function', () => {
+
+            expect(Users.isPasswordEqual).to.exist;
+            expect(Users.isPasswordEqual).to.be.a('function');
+        });
+
+        it('should return true if password match', () => {
+
+            const value = "123desafio";
+            const password = Users.generatePassword(value);
+
+            const result = Users.isPasswordEqual(value, password);
+
+            expect(result).to.be.a('boolean');
+            expect(result).to.be.true;
         });
     });
 });
