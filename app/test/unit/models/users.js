23267e1f0d52fda0c659c14cdad84fc4f3ddcd37
Filipe Botti <filipebotti@hotmail.com>
2017-01-15 11:55:30 -0300

    added test for generated password be md5 hash

--- app/test/unit/models/users.js
+++ app/test/unit/models/users.js
@@ -10,5 +10,14 @@ describe('Models: Users', () => {
             expect(Users.generatePassword).to.exist;
             expect(Users.generatePassword).to.be.a('function');
         });
+
+        it('should return a md5 hashed password for a input string', () => {
+
+            const password = Users.generatePassword('123desafio');
+
+            expect(password).to.exist;
+            expect(password).to.be.a('string');
+            expect(password).to.be.eql('D3BCEC7CE184C002B1CE7805CF36F333'); //md5 for 123desafio
+        });
     });
 });
