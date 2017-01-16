2a154866c9c44135d54e70369e659e90cc990810
Filipe Botti <filipebotti@hotmail.com>
2017-01-16 00:06:58 -0300

    fix message

--- app/shared/strings.js
+++ app/shared/strings.js
@@ -5,7 +5,7 @@ const content = {
     SENHA_FIELD_NOT_FOUND       : "A senha não foi informada",
     TELEFONE_FIELD_NOT_FOUND    : "Nenhum telefone foi informado",
     TELEFONE_FIELD_AT_LEAST_ONE : "Deve ser informado pelo menos um telefone",
-    TELEFONE_FIELD_NOT_VALID    : "Telefone(s) inválido(s), verifique o(s) telefone(s) informado(s), é necessario informa o número e o ddd.",
+    TELEFONE_FIELD_NOT_VALID    : "Telefone(s) inválido(s), verifique o(s) telefone(s) informado(s), é necessario informar o número e o ddd.",
     INTERNAL_SERVER_ERROR       : "Ops! Algo não funcionou como esperado.",
     USER_ALREADY_EXISTS         : "Email já existente",
     INVALID_CREDENTIALS         : "Usuário e/ou senha inválidos",
